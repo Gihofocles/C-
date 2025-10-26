212f2eac5af7bb9e73b0021029a62ba6765208d6
Gibran Hernandez Orozco <gibran.hzoz@gmail.com>
2025-10-26 06:00:23 -0600

    Añade punto de entrada main para el proyecto de aerolínea (Principalviaje)

---
+++ Principalviaje.cpp
@@ -0,0 +1,9 @@
+#include "viaje.hpp"
+
+int main()
+{
+  cout<<"Desarrollado por:\nHernandez Orozco Cesar Gibran\n1MV4\nEsponjositos\n\n";
+  system("pause");
+  Aerolineas s;
+  s.Calculaboleto();
+}
